fc0acf09f87818078a7da5ab26435b73ffa6886e
lucas ringuelet <lucasringuelet@outlook.com>
2022-05-16 15:13:44 -0300

    add assignament turn to gripe and fiebre. And add turns of user

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../models/user");
 const Vaccine = require("../models/vaccine");
 const Turn = require("../models/turn");
+const res = require("express/lib/response");
 
 
 
@@ -59,7 +60,9 @@ router.post("/assignVaccine/:id", async(req, res) => {
     res.json({ success: true, data: 'the vaccine was assigned' });
 })
 
-//asignar turno covid
+//asignar turno covid(primero se cargaria la parte de las vacunas que tiene, 
+//luego una vez ejecutado el assignVaccine para todas las vacunas que tiene, 
+//ejecutariamos este assignTurn que verificaria si hay que asignarle o no turno de en este caso covid).
 router.post("/assignTurnCovid/:id", async(req, res) => {
     const vaccine = "covid";
     const userSearch = await User.find({ _id: `${req.params.id}` })
@@ -101,12 +104,97 @@ router.post("/assignTurnCovid/:id", async(req, res) => {
 
 })
 
+//asignar turno gripe 
+router.post("/assignTurnGripe/:id", async(req, res) => {
+    const vaccine = "gripe";
+    const userSearch = await User.find({ _id: `${req.params.id}` })
+    const user = userSearch[0];
+    var today = new Date();
+    var year = today.getFullYear();
+    var dateBirthUser = new Date(user.dateBirth);
+    var age = (year - dateBirthUser.getFullYear());
+    //milisegundos de 1 mes
+    const month = 2629750000;
+
+
+    var arrayVaccination = await Vaccine.find({ _id: user.vaccinations });
+    var gripeVaccine = (arrayVaccination.filter(element => element.type === "gripe" && element.date != undefined));
+    var arrayDateOfVaccines = gripeVaccine.map((element) => element.date);
+    var dateMax = new Date(Math.max.apply(null, arrayDateOfVaccines));
+
+    var diff = today.getTime() - dateMax.getTime();
+    var days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    console.log(days)
+
+    if (days < 365) {
+        res.json({ success: false, error: "not a year passed" });
+    } else {
+        if (age < 60) {
+            var date = new Date((today.getTime() + (month * 6)));
+            var userId = user._id;
+            var state = true;
+            const turn = new Turn({ userId, vaccine, date, state });
+            await turn.save();
+            res.json({ success: true, data: date });
+        } else {
+            if (age >= 60) {
+                var date = new Date((today.getTime() + (month * 3)));
+                var userId = user._id;
+                var state = true;
+                const turn = new Turn({ userId, vaccine, date, state });
+                await turn.save();
+                res.json({ success: true, data: date });
+            }
+        }
+    }
+
+
+
+})
+
+router.post("/assignTurnFiebre/:id", async(req, res) => {
+    const vaccine = "fiebre";
+    const userSearch = await User.find({ _id: `${req.params.id}` })
+    const user = userSearch[0];
+    var today = new Date();
+    var year = today.getFullYear();
+    var dateBirthUser = new Date(user.dateBirth);
+    var age = (year - dateBirthUser.getFullYear());
+
+
+    var arrayVaccination = await Vaccine.find({ _id: user.vaccinations });
+    var amountVaccine = (arrayVaccination.filter(element => element.type === "fiebre").length);
+    if (amountVaccine > 0) {
+        res.json({ success: false, error: "user have 1 vaccine of fiebre" });
+    } else {
+        if (age < 60) {
+
+            res.json({ success: false, error: "user can't get vaccinated beacuse is minor than 60" });
+        } else {
+            var state = false;
+            var userId = req.params.id;
+            const turn = new Turn({ userId, vaccine, state });
+            await turn.save();
+            res.json({ success: true, data: "turn pennding to assinament" });
+        }
+    }
+
+
+
+})
+
 //vacunas dadas
 router.get("/vaccines/:id", async(req, res) => {
     const user = await User.find({ _id: req.params.id });
     const vaccines = await Vaccine.find({ _id: user[0].vaccinations })
     res.json(vaccines);
 });
+
+router.get("/turn/:id", async(req, res) => {
+    const user = await User.find({ _id: req.params.id });
+    const turns = await Turn.find({ userId: user[0]._id })
+    res.json({ success: true, data: turns });
+})
 
 
 module.exports = router;
